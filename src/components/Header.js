098386e2072ce2f50b1fd21c2ed8f9578027dfce
brhmz <ibrahimoz.mail@gmail.com>
2022-10-26 21:42:02 +0200

    City Detailes and Home Page finished.

---
+++ src/components/Header.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function header({user}) {
+  return (
+    <div className='header-container'>
+        <div className='nav-container'>
+            <a href = '/'>Home</a>
+            <a href = '/contactus'>Contact US</a>
+            <a href = '/aboutus'>About Us</a>
+        </div>
+        <div className='profile-container'>
+            <img className='profile-image' src = {user.imageUrl}/>
+            <p>{user.firstName}</p>
+        </div>
+    </div>
+  )
+}
+
+export default header
