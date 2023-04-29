a37ff8f1741a6afe5224029a267c9c16ab04f6fa
Ibrahim OZ <60812594+brhmz@users.noreply.github.com>
2023-04-29 23:38:49 +0200

    Update CityDetails.js

--- src/pages/CityDetails.js
+++ src/pages/CityDetails.js
@@ -8,7 +8,7 @@ function CityDetails({cities}) {
  
   return (
     <div className='city-details-container'>
-        <img className='detail-image' src = {currentCity[0].imageUrl}/>
+        <img className='detail-image' src = {currentCity[0].imageUrl} alt={currentCity.cityName}/>
         <h3>{currentCity[0].cityName.toUpperCase()}</h3>
         <p>{currentCity[0].description}</p>        
     </div>
