d2d1ffca9504b92ed33ad6f8edb588584b4a465b
Ibrahim OZ <60812594+brhmz@users.noreply.github.com>
2023-04-29 23:32:58 +0200

    Update CityCard.js

--- src/components/CityCard.js
+++ src/components/CityCard.js
@@ -3,7 +3,7 @@ import React from 'react'
 function CityCard({city}) {
   return (
     <div className='city-card'>
-        <img className='city-image' src = {city.imageUrl}/>
+        <img className='city-image' src = {city.imageUrl} alt={city.name}/>
         <h3>{city.cityName.toUpperCase()}</h3>
         <p>{city.description}</p>
         <a href = {`/citydetails/${city.cityName}`}><button>See Details</button></a>
