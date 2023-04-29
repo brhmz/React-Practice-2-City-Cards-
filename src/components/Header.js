55054dd65e2f715188e125fa786f9e03c11c42bc
Ibrahim OZ <60812594+brhmz@users.noreply.github.com>
2023-04-29 23:35:08 +0200

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,7 +9,7 @@ function header({user}) {
             <a href = '/aboutus'>About Us</a>
         </div>
         <div className='profile-container'>
-            <img className='profile-image' src = {user.imageUrl}/>
+            <img className='profile-image' src = {user.imageUrl} alt={use.userName}/>
             <p>{user.firstName}</p>
         </div>
     </div>
